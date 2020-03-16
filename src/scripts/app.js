c545be53bbc29c61c3c6480721c9c2f0b5067116
Abhay Vincent <abhaykvincent@gmail.com>
2020-03-16 12:07:11 -0400

    Splash structure ready

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -50,20 +50,45 @@ $(document).ready(function () {
         delay:2
     });
     var tl = gsap.timeline();
-    tl.to(".fullstack",{
+    tl.from(".splashSpot",{
+        opacity: 0,
+        duration: 0.25,delay:2.5
+    }).to(".fullstack",{
         x:87.2,
-        duration: 1,delay:2.5
+        duration: 0.25,delay:0.25
     }).to(".fullstack",{
         x:70,
-        duration: 0.5,delay:0.5
+        duration: 0.125,delay:0.5
     }).to(".fullstack",{
         x:42,
-        duration: 0.7,delay:0.5
+        duration: 0.175,delay:0.5
     }).
     
     from(".splashArrow",{
         height: 0,
         duration: 1
+    }).
+
+    from(".rect-1",{
+        scale: 0,
+        fill: "rgba(0,0,0,0)",
+        duration: 0.4,delay:0.05
+    }).from(".rect-2",{
+        scale: 0,
+        fill: "rgba(0,0,0,0)",
+        duration: 0.35,delay:0.05
+    }).from(".rect-3",{
+        scale: 0,
+        fill: "rgba(0,0,0,0)",
+        duration: 0.3,delay:0.05
+    }).from(".rect-4",{
+        scale: 0,
+        fill: "rgba(0,0,0,0)",
+        duration: 0.25,delay:0.05
+    }).from(".rect-5",{
+        scale: 0,
+        fill: "rgba(0,0,0,0)",
+        duration: 0.2,delay:0.05
     })
     
 });
